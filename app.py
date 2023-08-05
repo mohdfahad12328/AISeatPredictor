da9e72a12e93bc32507be02d18262fe1b60fe337
mohdfahad12328 <68467301+mohdfahad12328@users.noreply.github.com>
2023-08-05 13:57:01 +0530

    added uvicron to run directly

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import joblib
 from fastapi import FastAPI 
 from sklearn.tree import DecisionTreeClassifier
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,6 @@ def read_root(rank: int, caste: int):
     global model
     predictions = model.predict([[rank, caste]])
     return {"college": predictions[0][0], "branch": predictions[0][1]}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=3000, log_level="info")
--- main.py
+++
@@ -1,4 +0,0 @@
-import os
-
-if __name__ == "__main__":
-    os.system("uvicorn app:app --port 3000")
